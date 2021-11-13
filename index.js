e7d72c8fc54f0e81915514d7ad5cbb8c24e6b1fb
Bilas-Halder <bilas8642@gmail.com>
2021-11-13 15:45:48 +0600

    Users and buynow api added

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ async function run() {
         await client.connect();
         const database = client.db("Aqua-Ticking");
         const watchCollection = database.collection("watches");
+        const orderCollection = database.collection("orders");
+        const userCollection = database.collection("users");
 
         // Get API
         app.get('/watches', async (req, res) => {
@@ -30,6 +32,46 @@ async function run() {
             const watches = await cursor.toArray();
             res.json(watches);
         });
+        app.get('/watches/:id', async (req, res) => {
+            const id = req.params?.id;
+            const query = { _id: ObjectID(id) };
+            const watch = await watchCollection.findOne(query);
+            res.json(watch);
+        });
+
+        app.get('/users/role/:email', async (req, res) => {
+            const email = req.params?.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            res.json({ role: user?.role });
+        });
+
+        // post Api
+        app.post('/buyone', async (req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.json(newOrder);
+        });
+
+
+        // updating users
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+
+            const update = {
+                "$set": {
+                    ...user
+                }
+            };
+            const options = { "upsert": true };
+
+            console.log(user.email, "times");
+            const result = await userCollection.updateOne(query, update, options);
+            res.json(user);
+        });
+
+
 
     } finally {
         //   await client.close();
